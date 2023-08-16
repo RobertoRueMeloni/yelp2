818ef3c7529b3bc4744af5f46f900c6fc66b609f
Roberto Meloni <94394668+RobertoRueMeloni@users.noreply.github.com>
2023-08-16 11:43:07 -0700

    new component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Nav from './Components/Nav';
 import About from './Components/About';
 import SearchBar from './Components/Searchbar';
 import RestaurantsGeo from './Components/RestaurantsGeo';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <About />
       <SearchBar />
       <RestaurantsGeo />
+      <Login />
+      
     </>
   );
 }
